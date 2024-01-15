b1bc4bcb5224e93e5fa22259df3428dd5e564a3b
Matias <mmatidelga2@gmail.com>
2024-01-15 00:46:30 -0300

    Change lang html atributte

--- src/classes/Resend.ts
+++ src/classes/Resend.ts
@@ -1,9 +1,19 @@
 import { Resend } from "resend";
 
+/**
+ * Singleton class for managing the Resend instance.
+ */
+
 export class ResendSingleton {
   private static instance: any;
 
   private constructor() {}
+
+  /**
+   * Returns the singleton instance of Resend.
+   * If the instance does not exist, it creates a new one using the provided RESEND_API.
+   * @returns The Resend singleton instance.
+   */
 
   public static getInstance() {
     if (!ResendSingleton.instance) {
--- src/utils/formatAmount.ts
+++ src/utils/formatAmount.ts
@@ -1,3 +1,10 @@
+/**
+ * Formats a number as a currency value in USD.
+ *
+ * @param value - The number to format.
+ * @returns The formatted currency value.
+ */
+
 export const format = (value: number) => {
   const formatter = new Intl.NumberFormat("en-US", {
     style: "currency",
